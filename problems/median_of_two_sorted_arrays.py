37c3cea86ad7aed4816af40f51e2e38a898e3059
davkim1030 <davkim1030@gmail.com>
2025-05-26 19:44:43 +0900

    solve 4 Median of Two Sorted Arrays

--- problems/median_of_two_sorted_arrays.py
+++ problems/median_of_two_sorted_arrays.py
@@ -5,25 +5,31 @@ from typing import List
 
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        full_len = len(nums1) + len(nums2)
-        finding_len = full_len // 2 + 1
-        result = []
+        m, n = len(nums1), len(nums2)
+        full_length = m + n
+        if m > n:
+            return self.findMedianSortedArrays(nums2, nums1)
 
-        i1, i2 = 0, 0
-        while i1 + i2 < finding_len:
-            if len(nums1) == i1:
-                result.append(nums2[i2])
-                i2 += 1
-            elif len(nums2) == i2:
-                result.append(nums1[i1])
-                i1 += 1
-            elif nums1[i1] <= nums2[i2]:
-                result.append(nums1[i1])
-                i1 += 1
+        start, end = 0, m
+        while start <= end:
+            part1 = (start + end) // 2
+            part2 = (full_length + 1) // 2 - part1
+
+            max_left1 = nums1[part1 - 1] if part1 != 0 else float("-inf")
+            min_right1 = nums1[part1] if part1 != m else float("inf")
+
+            max_left2 = nums2[part2 - 1] if part2 != 0 else float("-inf")
+            min_right2 = nums2[part2] if part2 != n else float("inf")
+
+            if max_left1 <= min_right2 and max_left2 <= min_right1:
+                if full_length % 2 == 0:
+                    return (max(max_left1, max_left2) + min(min_right1, min_right2)) / 2
+                else:
+                    return max(max_left1, max_left2)
+            elif max_left1 > min_right2:
+                end -= 1
             else:
-                result.append(nums2[i2])
-                i2 += 1
+                start += 1
 
-        if full_len % 2 == 0:
-            return (result[-1] + result[-2]) / 2.0
-        return result[-1]
+        return 0.0
+
--- tests/test_median_of_two_sorted_arrays.py
+++ tests/test_median_of_two_sorted_arrays.py
@@ -1,44 +1,21 @@
-from unittest import TestCase
+import pytest
 
 from problems.median_of_two_sorted_arrays import Solution
-from tests import PSTestCase
 
 
-class TestMedianOfTwoSortedArrays(TestCase, PSTestCase):
-    def setUp(self):
-        self.solution_class = Solution()
-    def solution(self, *args, **kwargs) -> any:
-        return self.solution_class.findMedianSortedArrays(*args, **kwargs)
+solution = Solution()
 
-    def test(self):
-        # Given
-        nums1 = [1, 3]
-        nums2 = [2]
 
-        # When
-        result = self.solution(nums1, nums2)
-
-        # Then
-        self.assertEqual(result, 2.0)
-
-    def test2(self):
-        # Given
-        nums1 = [1, 2]
-        nums2 = [3, 4]
-
-        # When
-        result = self.solution(nums1, nums2)
-
-        # Then
-        self.assertEqual(result, 2.5)
-
-    def test3(self):
-        # Given
-        nums1 = [1]
-        nums2 = []
-
-        # When
-        result = self.solution(nums1, nums2)
-
-        # Then
-        self.assertEqual(result, 1.0)
+@pytest.mark.parametrize(
+    "nums1, nums2, expected", [
+        ([1, 3], [2], 2.0),
+        ([1, 2], [3, 4], 2.5),
+        ([1], [], 1.0),
+        ([1, 2, 3, 4, 5], [6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17], 9.0),
+        ([], [1, 2, 3, 4], 2.5),
+        ([1, 2, 3, 4], [], 2.5),
+    ]
+)
+def test_1(nums1, nums2, expected: float):
+    actual = solution.findMedianSortedArrays(nums1, nums2)
+    assert expected == actual
